d0c94a59c18518b73d0893fb694e67b3b0916720
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-11-12 16:31:48 -0300

    inicio projeto roadmap

---
+++ aspnetcore-template-identity/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace aspnetcore_template_identity
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
